25400607556440f1df81f400fd42a41239e9d3f3
GaryVT <gvilcat@ulasalle.edu.pe>
2025-01-19 17:53:05 -0500

    Puerto por defecto

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   server: {
-    port: 3000, // Cambia el puerto si es necesario 5173
+    port: 3000,
   },
-  // Otras configuraciones pueden ir aquí
+
 });
